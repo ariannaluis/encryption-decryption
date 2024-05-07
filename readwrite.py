426d51bb57749513cc6f37c7b448f920f3914991
ariannaluis <ariannaluis@gmail.com>
2024-05-07 19:20:43 -0400

    removed pad() and added documentation

--- readwrite.py
+++ readwrite.py
@@ -1,22 +1,22 @@
-from cryptography.hazmat.primitives import padding
+"""
+Contains read() and write() functions to read from and write to files
+respectively.
+
+Author: Arianna Luis
+"""
 
 
 def read(filename):
-    """reads plaintext from a file and returns it as a string"""
+    """
+    Read from file in binary mode.
+    """
     with open(filename, 'rb') as file:
         return file.read()
 
 
-def write(filename, text):
-    """writes the ciphertext to a file"""
-    with open(filename + '.enc', 'wb') as file:
-        file.write(text)
-
-
-def pad(data):
-    """pads plaintext to make its length a multiple of the block size
-    of the chosen cipher mode"""
-    padder = padding.PKCS7(128).padder()
-    padded = padder.update(data)
-    padded += padder.finalize()
-    return padded
+def write(filename, data):
+    """
+    Write to file in binary mode.
+    """
+    with open(filename, 'wb') as file:
+        file.write(data)
