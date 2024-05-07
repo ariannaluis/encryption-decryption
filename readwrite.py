f3fb4b332a7b8c18341dc6431c7e5f929b27d87c
ariannaluis <ariannaluis@gmail.com>
2024-05-07 18:15:32 -0400

    changed directories

--- readwrite.py
+++ readwrite.py
@@ -1,19 +1,22 @@
-def read_plaintext(filename):
-	"reads plaintext from a file and returns it as a string"
-	with open(filename, 'r') as file:
-		return file.read()
+from cryptography.hazmat.primitives import padding
 
-def write_ciphertext(filename, ciphertext):
-	"writes the ciphertext to a file"
-	with open(filename, 'w') as file:
-		file.write(ciphertext)
 
-def read_ciphertext(filename):
-	"reads ciphertext from a file and returns it as a string"
-	with open(filename, 'r') as file:
-		return file.read()
-	
-def write_plaintext(filename, plaintext):
-	"writes the plaintext to a file"
-	with open(filename, 'w') as file:
-		file.write(plaintext)
+def read(filename):
+    """reads plaintext from a file and returns it as a string"""
+    with open(filename, 'rb') as file:
+        return file.read()
+
+
+def write(filename, text):
+    """writes the ciphertext to a file"""
+    with open(filename + '.enc', 'wb') as file:
+        file.write(text)
+
+
+def pad(data):
+    """pads plaintext to make its length a multiple of the block size
+    of the chosen cipher mode"""
+    padder = padding.PKCS7(128).padder()
+    padded = padder.update(data)
+    padded += padder.finalize()
+    return padded
