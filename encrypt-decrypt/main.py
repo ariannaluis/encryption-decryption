39be314ef10943b203d09c61f4471544028e24d8
ariannaluis <ariannaluis@gmail.com>
2024-05-07 18:10:47 -0400

    added error handling

--- encrypt-decrypt/main.py
+++ encrypt-decrypt/main.py
@@ -5,17 +5,16 @@ import encrypt_decrypt as ed
 
 def main():
     action = input("Do you want to encrypt or decrypt a file? (e/d) >> ")
-    # print(action.lower())
-    # if action.lower() != "e" or action.lower() != "d":
-    #     raise ValueError("Invalid action. Please enter e for encryption or d for decryption.")
+    if action.lower() != "e" and action.lower() != "d":
+        raise ValueError("Invalid action. Please enter e for encryption or d for decryption.")
 
     filename = input("Enter the filename >> ")
-    # if not os.path.exists(filename):
-    #     raise FileNotFoundError(f"The file {filename} does not exist.")
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"The file {filename} does not exist.")
 
     aes = input("Enter the AES mode (cbc/ctr) >> ")
-    # if aes.lower() != "cbc" or aes.lower() != "ctr":
-    #     raise ValueError("Invalid AES mode. Valid AES modes are CBC or CTR.")
+    if aes.lower() != "cbc" and aes.lower() != "ctr":
+        raise ValueError("Invalid AES mode. Valid AES modes are CBC or CTR.")
 
     view = ""
 
@@ -29,7 +28,6 @@ def main():
         print("Invalid action")
         return
 
-    # output if requested
     if view == "y":
         print(readwrite.read(filename + '.enc'))
     else:
